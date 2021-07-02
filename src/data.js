a88ab0db1f46cab7ab79226ace8fd36c02a94a34
catplotlib <43911345+Puja2402@users.noreply.github.com>
2021-07-02 08:13:57 +0530

    upload code

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Puja Chaudhury',
+    num: 12457273196,
+    image:
+      'https://i.ibb.co/jWnWB0W/Whats-App-Image-2021-06-20-at-12-27-31-AM.jpg',
+  },
+  {
+    id: 2,
+    name: 'Abhinach Srivastava',
+    num: 3242344252,
+    image:
+      'https://i.ibb.co/dGtPgkv/as.jpg',
+  },
+  {
+    id: 3,
+    name: 'Arron Logan',
+    num: 234256576,
+    image:
+      'https://i.ibb.co/RCjq1tH/20210626-031724.jpg',
+  },
+  {
+    id: 4,
+    name: 'Tunni Sengupta',
+    num: 3757385835,
+    image:
+      'https://i.ibb.co/brMjhFY/20201225-160203.jpg',
+  },
+  {
+    id: 5,
+    name: 'Subho San',
+    num: 3265476758,
+    image:
+      'https://i.ibb.co/QjkS8G6/IMG-20200708-WA0083.jpg',
+  },
+];
